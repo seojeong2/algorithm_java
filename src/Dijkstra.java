6fdbe0f370c7c05f02beb520b9bc5be76b32d745
김서짱 <seojeong1101@gmail.com>
2023-07-17 00:13:01 +0900

    다익스트라 알고리즘 작성중

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,6 +1,35 @@
+import java.util.ArrayList;
+
 public class Dijkstra {
+
+    public static final int INF = (int)1e9;
+
+    // 노드의 개수(N), 간선의 개수(M), 시작 노드번호(start)
+    public static int n,m,start;
+
+    // 각 노드에 연결되어 있는 노드에 대한 정보를 담는 배열
+    public static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
 
     public static void main(String[] args) {
 
+
+
+    }
+}
+
+class Node {
+    private int index;
+    private int distance;
+
+    public Node(int index, int distance) {
+        this.index = index;
+        this.distance = distance;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+    public int getDistance() {
+        return this.distance;
     }
 }
