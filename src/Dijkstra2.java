0bb2cebda29f27a20b50b5532a0038cd4ebacc63
seojeong <seojeong1101@gmail.com>
2023-07-18 15:14:22 +0900

    다익스트라 알고리즘

--- src/Dijkstra2.java
+++ src/Dijkstra2.java
@@ -1,30 +1,29 @@
+import java.net.SecureCacheResponse;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Dijkstra {
+public class Dijkstra2 {
 
     public static final int INF = (int)1e9;
 
-    // 노드의 개수(N), 간선의 개수(M), 시작 노드번호(start)
+    // 노드의 개수:n, 간선의 개수:m, 시작 노드번호: start
     public static int n,m,start;
 
     // 각 노드에 연결되어 있는 노드에 대한 정보를 담는 배열
-    public static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
+    public static ArrayList<ArrayList<Node2>> graph = new ArrayList<ArrayList<Node2>>();
 
-    // 방문한 적이 있는지 체크하는 목적의 배열 만들기
+    // 방문체크 배열
     public static boolean[] visited = new boolean[100001];
-
-    // 최단거리 테이블 만들기
+    // 최단거리 테이블
     public static int[] d = new int[100001];
 
     // 방문하지 않은 노드 중에서, 가장 최단거리가 짧은 노드의 번호를 반환
     public static int getSmallestNode() {
         int min_value = INF;
-
         int index = 0;
 
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=n; i++) {
             if(d[i] < min_value && !visited[i]) {
                 min_value = d[i];
                 index = i;
@@ -33,8 +32,9 @@ public class Dijkstra {
         return index;
     }
 
+    // 다익스트라 알고리즘 수행
     public static void dijkstra(int start) {
-        // 시작 노드에 대해서 초기화
+        // 시작 노드 초기화
         d[start] = 0;
         visited[start] = true;
 
@@ -42,13 +42,11 @@ public class Dijkstra {
             d[graph.get(start).get(j).getIndex()] = graph.get(start).get(j).getDistance();
         }
 
-        // 시작 노드를 제외한 전체 n-1개의 노드에 대해서 반복
+        // 시작 노드제외 전체 n-1개의 노드에 대해서 반복
         for(int i=0; i<n-1; i++) {
-            // 현재 최단거리가 가장 짧은 노드를 꺼내서, 방문처리
             int now = getSmallestNode();
             visited[now] = true;
 
-            // 현재 노드와 연결된 다른 노드를 확인
             for(int j=0; j<graph.get(now).size(); j++) {
                 int cost = d[now] + graph.get(now).get(j).getDistance();
 
@@ -64,48 +62,45 @@ public class Dijkstra {
 
         Scanner sc = new Scanner(System.in);
 
-        n = sc.nextInt();
-        m = sc.nextInt();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
         start = sc.nextInt();
 
-        // 그래프 초기화
+        // 그래프초기화
         for(int i=0; i<=n; i++) {
-            graph.add(new ArrayList<Node>());
+            graph.add(new ArrayList<Node2>());
         }
 
         // 모든 간선 정보 입력받기
-        for (int i=0; i<m; i++){
+        for(int i=0; i<m; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             int c = sc.nextInt();
 
-            graph.get(a).add(new Node(b,c));
+            graph.get(a).add(new Node2(b,c));
         }
-
-        // 최단거리 테이블을 모두 무한으로 초기화
+        // 최단거리 테이블 무한으로 초기화
         Arrays.fill(d,INF);
-
-        // 다익스트라 알고리즘을 수행
+        // 다익스트라 알고리즘 수행
         dijkstra(start);
 
-        // 모든 노드로 가기위한 최단 거리를 출력
+        // 모든 노드로 가기 위한 최단 거리 출력
         for(int i=1; i<=n; i++) {
-            // 도달할 수 없는 경우, 무한으로 출력
             if(d[i] == INF) {
                 System.out.println("INFINITY");
             } else {
                 System.out.println(d[i]);
             }
         }
-
     }
 }
 
-class Node {
-    private int index;
-    private int distance;
+class Node2 {
+    public int index;
+    public int distance;
 
-    public Node(int index, int distance) {
+    public Node2(int index, int distance) {
         this.index = index;
         this.distance = distance;
     }
